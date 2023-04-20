5aaf67794e9d9a5185ce3a79a72f0fdb9a68b92a
fischl05 <129149925+fischl05@users.noreply.github.com>
2023-04-20 20:34:32 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	int a, b, c;
+
+	scanf("%d %d %d", &a, &b, &c);
+
+	if (a + b + c == 180) {
+		if (a == 60 && b == 60 && c == 60) printf("Equilateral");
+		else if (a == b || b == c || a == c) printf("Isosceles");
+		else printf("Scalene");
+	}
+	else printf("Error");
+
+	return 0;
+}
