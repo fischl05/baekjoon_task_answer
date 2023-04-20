fe3d55299f653f0b13fb194f0d36a728a30eca6a
fischl05 <129149925+fischl05@users.noreply.github.com>
2023-04-20 21:46:45 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main(void) {
+	/*int a;
+	scanf("%d", &a);
+	printf("1\n0");*/
+
+	uint64_t n;
+
+	scanf("%lld", &n);
+	printf("%lld\n2", n * n);
+
+	return 0;
+}
