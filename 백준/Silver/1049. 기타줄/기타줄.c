5378036f01433d0aff1f917f42bd2a0d1e48ee9a
fischl05 <129149925+fischl05@users.noreply.github.com>
2023-04-23 15:34:27 +0900

    [Silver IV] Title: 기타줄, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/1049. 기타줄/기타줄.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+	int six;
+	int one;
+}GUITAR;
+
+int compare_six(const void* mem1, const void* mem2) {
+	GUITAR* a = mem1;
+	GUITAR* b = mem2;
+	return a->six - b->six;
+}
+
+int compare_one(const void* mem1, const void* mem2) {
+	GUITAR* a = mem1;
+	GUITAR* b = mem2;
+	return a->one - b->one;
+
+}
+
+int main(void) {
+	int n, m;
+	int idx[2] = { 0 };
+	int use_money = 0;
+
+	scanf("%d %d", &n, &m);
+	GUITAR* cost = (GUITAR*)calloc(sizeof(GUITAR), m);
+
+	for (int i = 0; i < m; i++) {
+		scanf("%d %d", &cost[i].six, &cost[i].one);
+		if (cost[idx[0]].six > cost[i].six) idx[0] = i;
+		if (cost[idx[1]].one > cost[i].one) idx[1] = i;
+	}
+	
+	while (n > 0) {
+		int multi = n;
+		if (multi > 6) multi = 6;
+		if (cost[idx[0]].six <= cost[idx[1]].one * multi) {
+			use_money += cost[idx[0]].six;
+		}
+		else {
+			use_money += cost[idx[1]].one * multi;
+		}
+		n -= 6;
+	}
+
+	printf("%d", use_money);
+
+	free(cost);
+	return 0;
+}
