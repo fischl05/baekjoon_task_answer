858734948605e4dbaef9339bbd9d254e15079bfa
fischl05 <129149925+fischl05@users.noreply.github.com>
2023-06-17 14:15:10 +0900

    [Silver IV] Title: 정수 제곱근, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/2417. 정수 제곱근/정수 제곱근.c
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <math.h>
 
-int main()
-{
-    unsigned long long n, r;
-    scanf("%lld",&n);
-    r = (long long)sqrt((double)n); r-=(r>0);
-    while(r*r < n) r++;
-    printf("%llu\n", r);
+int main(void) {
+	uint64_t num, start, last, middle/*, idx = INT64_MAX*/;
+
+	scanf("%lld", &num);
+
+	start = 0, last = 3037000500;
+	//idx = sqrt(num);
+
+	while (start <= last) {
+		middle = (start + last) / 2;
+		if (middle * middle > num) last = middle - 1;
+		else if (middle * middle < num)  start = middle + 1;
+		else break;
+	}
+	if (num > middle * middle) middle++;
+	printf("%lld\n", middle);
+
+	return 0;
 }
